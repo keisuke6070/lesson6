02ef28638befafc46d2eabee8dea215b4ec49d8b
Keisuke Yoshida <kskysd0706@gmail.com>
2022-04-02 12:38:01 +0000

    [finish]6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,17 @@
 class BookCommentsController < ApplicationController
  def create
-  book = Book.find(params[:book_id])
-  comment = BookComment.new(book_comment_params)
-  comment.user_id = current_user.id
-  comment.book_id = book.id
-  comment.save
-  redirect_to request.referer
+  @book = Book.find(params[:book_id])
+  @comment = current_user.book_comments.new(book_comment_params)
+  @comment.book_id = @book.id
+  @comment.save
+  render :index
+
  end
 
  def destroy
-  BookComment.find(params[:id]).destroy
-  redirect_to request.referer
+  BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+  @book = Book.find(params[:book_id])
+  render :index
  end
 
  private
@@ -20,3 +21,4 @@ class BookCommentsController < ApplicationController
  end
 
 end
+
